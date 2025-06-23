effd34bb6cf27d80c99a90e4c8a8d2c7917ee679
agnusmor <agnusmor@gmail.com>
2025-06-23 16:31:19 +0200

    Zisk patch (sha2 v0.10.9)

--- sha2/src/sha256.rs
+++ sha2/src/sha256.rs
@@ -1,7 +1,9 @@
 use digest::{generic_array::GenericArray, typenum::U64};
 
 cfg_if::cfg_if! {
-    if #[cfg(feature = "force-soft-compact")] {
+    if #[cfg(all(target_os = "zkvm", target_vendor = "zisk"))] {
+        use ziskos::zisklib::sha256f_compress as compress;
+    } else if #[cfg(feature = "force-soft-compact")] {
         mod soft_compact;
         use soft_compact::compress;
     } else if #[cfg(feature = "force-soft")] {
